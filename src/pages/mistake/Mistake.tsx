4ddcc952db070fc8261b39f325fa9d1207dfa5f5
mlh <1623369197@qq.com>
2025-12-17 22:24:17 +0800

    错题本

--- src/pages/mistake/Mistake.tsx
+++ src/pages/mistake/Mistake.tsx
@@ -1,9 +1,10 @@
 import Options from '@/components/options/Options'
 import type { RootState } from '@/store'
-import { useEffect, useMemo } from 'react'
+import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import style from './Mistake.module.scss'
+import { Flex, Button } from 'antd'
 
 
 const Mistake = () => {
@@ -11,19 +12,25 @@ const Mistake = () => {
   const examHistory = useSelector((s: RootState) => s.history.historyRecord)
   const navigate = useNavigate()
 
+  const mistakeTitle = useMemo(() => {
+    const id = Number(searchParams.get('id'))
+    return examHistory.find(v => v.id === id)
+  }, [examHistory, searchParams])
+
   const mistakeInfo = useMemo(() => {
     const id = Number(searchParams.get('id'))
     return examHistory.find(v => v.id === id)?.list.filter(item => item.myAnswer !== item.result)
   }, [examHistory, searchParams])
 
-  useEffect(() => {
-    if (examHistory.length === 0) {
-      navigate('/')
-    }
-  }, [examHistory])
-
   return (
     <div className={style.mistake}>
+      <Flex justify="space-between" style={{marginBottom: 20}}>
+        <h1 className={style.title}>
+          错题本
+          <p>考试时间:{new Date(mistakeTitle!.id).toLocaleString()}|考试得分:{mistakeTitle?.score}分|错题数量:{mistakeTitle?.errorCount}题</p>
+        </h1>
+        <Button onClick={() => navigate('/history')}>返回记录</Button>
+      </Flex>
       {mistakeInfo?.map((item, index) => 
         <Options 
           key={item.question}
